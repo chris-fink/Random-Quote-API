a157872960459008c2f9a230aeabaa36c61db6e4
Chris Fink <65560083+chris-fink@users.noreply.github.com>
2022-08-12 16:04:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-
 //New API try
-let text = document.getElementById("text");
-let author = document.getElementById("author");
-let btn = document.getElementById("btn");
-
 const settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://famous-quotes4.p.rapidapi.com/random?category=all&count=1&rapidapi-key=908b164f23mshb216b4faf70a4d5p157706jsn815bc63f9c9d",
 	"method": "GET",
 	"headers": {
 		"X-RapidAPI-Key": "908b164f23mshb216b4faf70a4d5p157706jsn815bc63f9c9d",
 		"X-RapidAPI-Host": "famous-quotes4.p.rapidapi.com"
 	}
 };
-
+const url = "https://famous-quotes4.p.rapidapi.com/random?category=all&count=1&rapidapi-key=908b164f23mshb216b4faf70a4d5p157706jsn815bc63f9c9d"
 
 let getQuote = () => {
-  fetch()
-    .then((data) => data.json())
-    .then((item) => {
-      quote.innerText = item.content;
-      author.innerText = item.author;
-    });
-};
+	fetch(url, settings)
+	  .then(response =>response.json())
+	  .then((response) => {
+		quote = response[0]['text'];
+		author = response[0]['author'];
+	  })
+	  .then(log => {
+		console.log(quote);
+		console.log(author);
+	  })
+	  document.getElementById("text").innerHTML = quote;
+	  document.getElementById("author").innerHTML = author;
+  };
 
+  let btn = document.getElementById("btn");
 
 //DOM to load quote with button click
 window.addEventListener("load", getQuote);
